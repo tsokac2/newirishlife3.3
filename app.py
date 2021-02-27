0b6bb199652f2050587a3e518310ae0ed9528384
tsokac2 <tsokac2@gmail.com>
2021-02-27 23:40:51 +0000

    First test

--- app.py
+++ app.py
@@ -170,4 +170,4 @@ def delete_user():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
