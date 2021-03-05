16d4e0b125bf405a30a3716e22b253e9dd5d8ffb
tsokac2 <tsokac2@gmail.com>
2021-03-05 10:04:49 +0000

    add

--- app.py
+++ app.py
@@ -166,12 +166,15 @@ def delete_user():
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html', error=error), 404
+    flash("ERROR 404 - Page Not Found", "error-404")
+    return render_template ("home.html", error=error), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html', error=error), 500
+    flash("ERROR 500 - Page Not Found", "error-500")
+    return render_template ("home.html", error=error), 500
+   
 
 
 if __name__ == "__main__":
