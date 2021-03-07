2a0cc686f2b2169eb6a07372c6da9486eef663d2
tsokac2 <tsokac2@gmail.com>
2021-03-07 14:00:00 +0000

    add

--- static/js/maps.js
+++ static/js/maps.js
@@ -13,103 +13,77 @@ google.maps.event.trigger(map, "resize");
 map.setCenter(center);
 });
 
-    var busMarkerIcon = {
-        url:"static/images/bus_marker.png",
-        scaledSize: new google.maps.Size(50, 50)
-    };
+        var busMarkerIcon = {
+            url:"static/images/bus_marker.png",
+            scaledSize: new google.maps.Size(50, 50)
+        };
 
-    var dartMarkerIcon = {
-        url:"static/images/dart_marker.png",
-        scaledSize: new google.maps.Size(50, 50)
-    };
+        var dartMarkerIcon = {
+            url:"static/images/dart_marker.png",
+            scaledSize: new google.maps.Size(50, 50)
+        };
 
-    var luasMarkerIcon = {
-        url:"static/images/luas_marker.png",
-        scaledSize: new google.maps.Size(50, 50)
-    };
+        var luasMarkerIcon = {
+            url:"static/images/luas_marker.png",
+            scaledSize: new google.maps.Size(50, 50)
+        };
 
-    var Bus747Stop = new google.maps.Marker({
-        position: {lat: 53.42810, lng: -6.24407},
-        map: map,
-        title: "BUS 747 Stop",
-        icon: busMarkerIcon
-    });
+      
 
-    var BusarasBusStation = new google.maps.Marker({
-        position: {lat: 53.34983, lng: -6.25185},
-        map: map,
-        title: "Busaras Central Bus Station",
-        icon: busMarkerIcon
-    });
+        var TaraStation = new google.maps.Marker({
+            position: {lat: 53.34756, lng: -6.25418},
+            map: map,
+            title: "Tara Station, Bus Stop at Train Station",
+            icon: dartMarkerIcon
+        });
 
-    var HeustonStation = new google.maps.Marker({
-        position: {lat: 53.34646, lng: -6.29413},
-        map: map,
-        title: "Heuston Station",
-        icon: dartMarkerIcon
-    });
+        var AbbeyStreetLuas = new google.maps.Marker({
+            position: {lat: 53.34871, lng: -6.25819},
+            map: map,
+            title: "Abbey Street Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var ConnollyTrainStation = new google.maps.Marker({
-        position: {lat: 53.35165, lng: -6.24970},
-        map: map,
-        title: "Busaras Central Station",
-        icon: dartMarkerIcon
-    });
+        var TrinityLuasStop = new google.maps.Marker({
+            position: {lat: 53.34540, lng: -6.25826},
+            map: map,
+            title: "Trinity Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var TaraStation = new google.maps.Marker({
-        position: {lat: 53.34756, lng: -6.25418},
-        map: map,
-        title: "Tara Station, Bus Stop at Train Station",
-        icon: dartMarkerIcon
-    });
+        var TallaghtLuasStop = new google.maps.Marker({
+            position: {lat: 53.28752, lng: -6.37463},
+            map: map,
+            title: "Tallaght Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var AbbeyStreetLuas = new google.maps.Marker({
-        position: {lat: 53.34871, lng: -6.25819},
-        map: map,
-        title: "Abbey Street Luas Stop",
-        icon: luasMarkerIcon
-    });
+        var SaggartLuasStop = new google.maps.Marker({
+            position: {lat: 53.28481, lng: -6.43776},
+            map: map,
+            title: "Saggart Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var TrinityLuasStop = new google.maps.Marker({
-        position: {lat: 53.34540, lng: -6.25826},
-        map: map,
-        title: "Trinity Luas Stop",
-        icon: luasMarkerIcon
-    });
+        var ThePoint = new google.maps.Marker({
+            position: {lat: 53.34848, lng: -6.22894},
+            map: map,
+            title: "The Point Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var TallaghtLuasStop = new google.maps.Marker({
-        position: {lat: 53.28752, lng: -6.37463},
-        map: map,
-        title: "Tallaght Luas Stop",
-        icon: luasMarkerIcon
-    });
+        var BridesGlen = new google.maps.Marker({
+            position: {lat: 53.24204, lng: -6.14275},
+            map: map,
+            title: "Brides Glen Luas Stop",
+            icon: luasMarkerIcon
+        });
 
-    var SaggartLuasStop = new google.maps.Marker({
-        position: {lat: 53.28481, lng: -6.43776},
-        map: map,
-        title: "Saggart Luas Stop",
-        icon: luasMarkerIcon
-    });
-
-    var ThePoint = new google.maps.Marker({
-        position: {lat: 53.34848, lng: -6.22894},
-        map: map,
-        title: "The Point Luas Stop",
-        icon: luasMarkerIcon
-    });
-
-    var BridesGlen = new google.maps.Marker({
-        position: {lat: 53.24204, lng: -6.14275},
-        map: map,
-        title: "Brides Glen Luas Stop",
-        icon: luasMarkerIcon
-    });
-
-    var Broombridge = new google.maps.Marker({
-        position: {lat: 53.37234, lng: -6.29772},
-        map: map,
-        title: "Broombridge Luas Stop",
-        icon: luasMarkerIcon
-    });
+        var Broombridge = new google.maps.Marker({
+            position: {lat: 53.37234, lng: -6.29772},
+            map: map,
+            title: "Broombridge Luas Stop",
+            icon: luasMarkerIcon
+        });
 
 }
